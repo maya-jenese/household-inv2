ab41ca423487b8e08231a4fdcfa9d1b77c409af4
Maya Brown <maya.brown1207@gmail.com>
2022-07-19 09:21:15 -0400

    Successful login attempt

--- client/src/components/Main/index.jsx
+++ client/src/components/Main/index.jsx
@@ -1,21 +1,23 @@
+//import { User } from "../../../../server/models/user";
 import styles from "./styles.module.css";
 
 const Main = () => {
-	const handleLogout = () => {
-		localStorage.removeItem("token");
-		window.location.reload();
-	};
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  };
 
-	return (
-		<div className={styles.main_container}>
-			<nav className={styles.navbar}>
-				<h1>fakebook</h1>
-				<button className={styles.white_btn} onClick={handleLogout}>
-					Logout
-				</button>
-			</nav>
-		</div>
-	);
+  return (
+    <div className={styles.main_container}>
+      <nav className={styles.navbar}>
+        <h1>Household Inventory System</h1>
+        <button className={styles.white_btn} onClick={handleLogout}>
+          Logout
+        </button>
+      </nav>
+	  <h2> User's Properties</h2>
+    </div>
+  );
 };
 
 export default Main;
--- server/routes/users.js
+++ server/routes/users.js
@@ -12,7 +12,7 @@ router.post("/", async (req, res) => {
 		if (user)
 			return res
 				.status(409)
-				.send({ message: "User with given email already Exist!" });
+				.send({ message: "User with given email already exists!" });
 
 		const salt = await bcrypt.genSalt(Number(process.env.SALT));
 		const hashPassword = await bcrypt.hash(req.body.password, salt);
