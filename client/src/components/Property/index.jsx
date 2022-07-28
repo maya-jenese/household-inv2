7095e1449bff6cba4c45240c99f71889954f30e3
Maya Brown <maya.brown1207@gmail.com>
2022-07-27 21:20:06 -0400

    Render all properties

--- client/src/components/Property/index.jsx
+++ client/src/components/Property/index.jsx
@@ -1,149 +1,165 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./styles.module.css";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import logo from "../../logo_normal.png";
 import axios from "axios";
 
-
-
 const Property = () => {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const [error, setError] = useState("");
-	// const [data, setData] = useState("");
-	const [userData, setUserData] = useState({});
+  // useEffect(() => {
+  // 	fetch("http://localhost:8080/api/property")
+  // 		.then(res => res.json())
+  // 		.then(jsonRes => setProperties(jsonRes));
+  // 	console.log("HERE")
+  // 	console.log(properties);
+  // }, []);
 
-	// tracks property state w/ 'properties'
-	const [properties, setProperties] = useState([{
-		user_id: '',
-		property_description: '',
-		property_cost: 0,
-		property_quantity: 0
-	}]);
+  const navigateToProfile = () => {
+    navigate("/userprofile");
+  };
 
-	// useEffect(() => {
-	// 	fetch("http://localhost:8080/api/property")
-	// 		.then(res => res.json())
-	// 		.then(jsonRes => setProperties(jsonRes));
-	// 	console.log("HERE")
-	// 	console.log(properties);
-	// }, []);
+  const navigateToProperty = () => {
+    navigate("/property");
+  };
 
-	const navigateToProfile = () => {
-		navigate('/userprofile');
-	};
+  const navigateToAddProperty = () => {
+    navigate("/add-property");
+  };
 
-	const navigateToProperty = () => {
-		navigate('/property');
-	};
+  const navigateToAdmin = () => {
+    navigate("/admin");
+  };
 
-	const navigateToAddProperty = () => {
-		navigate('/add-property');
-	};
+  const navigateHome = () => {
+    navigate("/");
+  };
 
-	const navigateToAdmin = () => {
-		navigate('/admin');
-	};
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  };
 
-	const navigateHome = () => {
-		navigate('/');
-	};
+  //const [error, setError] = useState("");
+  const [data, setData] = useState("");
+  //const [userData, setUserData] = useState("");
 
-	const handleLogout = () => {
-		localStorage.removeItem("token");
-		window.location.reload();
-	};
+  // tracks property state w/ 'properties'
+  const [properties, setProperties] = useState([
+    {
+      property_description: "",
+      property_cost: 0,
+      property_quantity: 0,
+    },
+  ]);
 
-	// useEffect(() => {
-	// 	fetch("http://localhost:8080/api/users/getuserinfo", {
-	// 		headers: {"Content-Type": "application/json"},
-	// 		method: "POST",
-	// 		body: JSON.stringify({token: localStorage.getItem("token")})
-	// 	}).then(response => response.json()).then(data => setData(data));
-	// }, []);
+  useEffect(() => {
+    fetch("http://localhost:8080/api/users/getuserinfo", {
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+      body: JSON.stringify({ token: localStorage.getItem("token") }),
+    })
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
 
+  // Gets user data
+  //   useEffect(() => {
+  //     fetch("http://localhost:8080/api/users/getuserinfo", {
+  //       headers: { "Content-Type": "application/json" },
+  //       method: "POST",
+  //       body: JSON.stringify({ token: localStorage.getItem("token") }),
+  //     })
+  //       .then((response) => response.json())
+  //       .then((userData) => setUserData(userData));
+  //   }, []);
 
-	// Gets user data
-	useEffect(() => {
-		fetch("http://localhost:8080/api/users/getuserinfo", {
-			headers: {"Content-Type": "application/json"},
-			method: "POST",
-			body: JSON.stringify({token: localStorage.getItem("token")})
-		}).then(response => response.json()).then(userData => setUserData(userData));
-	}, []);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/api/property/get-properties")
+      .then((res) => {
+        setProperties(res.data.data.userProperties);
+      });
 
-	useEffect(() => {
-		// axios.get('http://localhost:8080/property').then(res => {
-		// 	setProperties(res.data.data.properties);
-		// })
+    // axios.get('http://localhost:8080/property')
+    // 	.then(res => res.json())
+    // 	.then(jsonRes => setProperties(jsonRes));
 
-		// axios.get('http://localhost:8080/property')
-		// 	.then(res => res.json())
-		// 	.then(jsonRes => setProperties(jsonRes));
+    // try {
+    //   properties.user_id = userData._id;
+    //   console.log(properties);
+    //   const url = "http://localhost:8080/api/property"; // api url that you're trying to access
+    //   const { data: res } = axios.get(url); // 2nd param = info getting put into req.body
+    //   console.log(res.message);
+    // } catch (error) {
+    //   if (
+    //     error.response &&
+    //     error.response.status >= 400 &&
+    //     error.response.status <= 500
+    //   ) {
+    //     setError(error.response.data.message);
+    //   }
+    // }
+  }, []);
 
-		try {
-			properties.user_id = userData._id;
-			  console.log(properties);
-			  const url = "http://localhost:8080/api/property"; // api url that you're trying to access
-			  const { data: res } = axios.get(url, properties); // 2nd param = info getting put into req.body
-			  console.log(res.message);
-		  } catch (error) {
-			  if (
-				  error.response &&
-				  error.response.status >= 400 &&
-				  error.response.status <= 500
-			  ) {
-				  setError(error.response.data.message);
-			  }
-		  }
-	}, []);
-	
-	//Set the page tab title
-	useEffect(() => {
-		document.title = "Your Property - West Boca Make-Believe Retirement Community"
-	});
+  //Set the page tab title
+  useEffect(() => {
+    document.title =
+      "Your Property - West Boca Make-Believe Retirement Community";
+  });
 
-	return (
-		<div className={styles.main_container}>
-			<nav className={styles.navbar}>
-				<img id={styles.logo} src={logo}/>
-				<h1>West Boca Make-Believe Retirement Community</h1>
-				<div className={styles.buttons}>
-					<button className={styles.white_btn} onClick={navigateHome}>
-						Home
-					</button>
-					<button className={styles.white_btn} onClick={navigateToProperty}>
-						Property
-					</button>
-					<button className={styles.white_btn} onClick={navigateToProfile}>
-						Profile
-					</button>
-					<button hidden={!userData.isAdmin} className={styles.white_btn} onClick={navigateToAdmin}>
-						Admin
-					</button>
-					<button className={styles.white_btn} id={styles.red_hover} onClick={handleLogout}>
-						Logout
-					</button>
-				</div>
-			</nav>
-			<div className={styles.property_list}>
-				<h2>My Properties</h2>
-					{
-						properties.map((property, key) => {
-						<div key={key}>
-						<h4>Description: {property.property_description}</h4>
-						<p>Cost: {property.property_cost}</p>
-						<p>Qty: {property.property_quantity}</p>
-						</div>
-						})
-					}
-					</div>
-				<button onClick={navigateToAddProperty}>Add Property</button>
-			<footer>
-				<h1>All rights reserved © copyright 2022, West Boca Make-Believe Retirement Community</h1>
-			</footer>
-		</div>
-	);
+  return (
+    <div className={styles.main_container}>
+      <nav className={styles.navbar}>
+        <img id={styles.logo} src={logo} />
+        <h1>West Boca Make-Believe Retirement Community</h1>
+        <div className={styles.buttons}>
+          <button className={styles.white_btn} onClick={navigateHome}>
+            Home
+          </button>
+          <button className={styles.white_btn} onClick={navigateToProperty}>
+            Property
+          </button>
+          <button className={styles.white_btn} onClick={navigateToProfile}>
+            Profile
+          </button>
+          <button
+            hidden={!data.isAdmin}
+            className={styles.white_btn}
+            onClick={navigateToAdmin}
+          >
+            Admin
+          </button>
+          <button
+            className={styles.white_btn}
+            id={styles.red_hover}
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
+        </div>
+      </nav>
+      <div className={styles.property_list}>
+        <h2>My Properties</h2>
+        <button onClick={navigateToAddProperty}>Add Property</button>
+        {properties.map((property, key) => {
+          return (
+            <div key={key}>
+              <h4>Description: {property.property_description}</h4>
+              <p>Cost: {property.property_cost}</p>
+              <p>Qty: {property.property_quantity}</p>
+            </div>
+          );
+        })}
+      </div>
+      <footer>
+        <h1>
+          All rights reserved © copyright 2022, West Boca Make-Believe
+          Retirement Community
+        </h1>
+      </footer>
+    </div>
+  );
 };
 
 export default Property;
--- server/routes/property.js
+++ server/routes/property.js
@@ -1,79 +1,79 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const jwt = require("jsonwebtoken");
+//const jwt = require("jsonwebtoken");
 const { User } = require("../models/user");
 
 // Load Property model
-const Property = require('../models/property');
+const Property = require("../models/property");
 
 // @route GET api/properties/test
 // @description tests properties route
 // @access Public
-router.get('/test', (req, res) => res.send('property route testing!'));
+router.get("/test", (req, res) => res.send("property route testing!"));
 
 // @route GET api/properties
 // @description Get all properties
 // @access Public
-router.get('/', async (req, res) => {
+router.get("/get-properties", async (req, res) => {
   // Property.find()
   //   .then(properties => res.json(properties))
   //   .catch(err => res.status(404).json({ message: 'No Properties found' }));
   //console.log("Here");
-  
-  console.log("HERE")
-  console.log(req.body);
-  const userProperties = await User.findById(req.body.user_id);
+
+  const userProperties = await Property.find({});
+  //console.log(userProperties);
   try {
     res.status(200).json({
-      status: 'Success',
+      status: "Success",
       data: {
-        userProperties
-      }
-    })
+        userProperties,
+      },
+    });
   } catch (err) {
     res.status(500).json({
-      status: 'Failed',
-      message: err
-    })
+      status: "Failed",
+      message: err,
+    });
   }
-  // User.findById(req.body.user_id).find("properties", function(err, result) {
-  //   if (err) {
-  //     res.send(err);
-  //   } else {
-  //     res.json(result);
-  //   }
-  // })
-    // .then(propInfo => res.json(propInfo.properties))
-    // .catch (err => res.status(404).json({ message: 'No Property found'}));
 });
 
 // @route GET api/books/:id
 // @description Get single book by id
 // @access Public
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   Property.findById(req.params.id)
-    .then(property => res.json(property))
-    .catch(err => res.status(404).json({ message: 'No Property found' }));
+    .then((property) => res.json(property))
+    .catch((err) => res.status(404).json({ message: "No Property found" }));
 });
 
 // @route GET api/properties
 // @description add/save property
 // @access Public
-router.post('/', async (req, res) => {
-  const propertyToAdd = new Property({ ...req.body, property_description: req.body.property_description, property_cost: req.body.property_cost, property_quantity: req.body.property_quantity });
+router.post("/", async (req, res) => {
+  const propertyToAdd = new Property({
+    ...req.body,
+    property_description: req.body.property_description,
+    property_cost: req.body.property_cost,
+    property_quantity: req.body.property_quantity,
+  });
 
   //const tokenContents = jwt.verify(req.body.token, `${process.env.JWTPRIVATEKEY}`);
 
-  User.findByIdAndUpdate(req.body.user_id, { "$push": {
-    properties: [propertyToAdd._id]
-  }},
-  function(err, user) {
-    if (err) {
-      console.log(err)
-    } else {
-      res.status(500).send({ message: "Property successfully added" });
+  User.findByIdAndUpdate(
+    req.body.user_id,
+    {
+      $push: {
+        properties: [propertyToAdd._id],
+      },
+    },
+    function (err, user) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.status(500).send({ message: "Property successfully added" });
+      }
     }
-  })
+  );
 
   await propertyToAdd.save();
 });
@@ -81,21 +81,21 @@ router.post('/', async (req, res) => {
 // @route GET api/properties/:id
 // @description Update property
 // @access Public
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   Property.findByIdAndUpdate(req.params.id, req.body)
-    .then(property => res.json({ msg: 'Updated successfully' }))
-    .catch(err =>
-      res.status(400).json({ error: 'Unable to update the Database' })
+    .then((property) => res.json({ msg: "Updated successfully" }))
+    .catch((err) =>
+      res.status(400).json({ error: "Unable to update the Database" })
     );
 });
 
 // @route GET api/properties/:id
 // @description Delete properties by id
 // @access Public
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   Book.findByIdAndRemove(req.params.id, req.body)
-    .then(book => res.json({ mgs: 'Property entry deleted successfully' }))
-    .catch(err => res.status(404).json({ error: 'No such a book' }));
+    .then((book) => res.json({ mgs: "Property entry deleted successfully" }))
+    .catch((err) => res.status(404).json({ error: "No such a book" }));
 });
 
 module.exports = router;
