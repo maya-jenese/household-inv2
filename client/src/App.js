de76c1b4493f50ff4807881c383a140101e21069
Maya Brown <maya.brown1207@gmail.com>
2022-07-29 22:53:19 -0400

    Firebase tings

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,28 @@ import Property from "./components/Property";
 import Admin from "./components/Admin";
 import UpdateProperty from "./components/UpdateProperty";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC0ILE9K94zgkAWr-rueMzgwwfj1hx29o0",
+  authDomain: "household-inventory-syst-f39e7.firebaseapp.com",
+  projectId: "household-inventory-syst-f39e7",
+  storageBucket: "household-inventory-syst-f39e7.appspot.com",
+  messagingSenderId: "806507679584",
+  appId: "1:806507679584:web:c584e58813b40422781805",
+  measurementId: "G-LST1D3NXPY",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
   const user = localStorage.getItem("token");
 
